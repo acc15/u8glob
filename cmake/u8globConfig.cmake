923897ad2b76f55321001755cad4dacf54dcaa96
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2024-04-07 05:20:58 +0300

    utfcpp added as submodule due to cmake support deprecation

--- cmake/u8globConfig.cmake
+++
@@ -1,4 +0,0 @@
-include(CMakeFindDependencyMacro)
-find_dependency(utf8cpp)
-
-include("${CMAKE_CURRENT_LIST_DIR}/u8globTargets.cmake")
---
+++ install/u8globConfig.cmake
@@ -0,0 +1,1 @@
+include("${CMAKE_CURRENT_LIST_DIR}/u8globTargets.cmake")
