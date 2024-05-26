a218b8f6312be632fc776a32e4efafcb4644b49f
acc15 <vyacheslav.mayorov@gmail.com>
2024-05-26 21:23:10 +0300

    fix msvc build

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ Toolchain="${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake"
 
 rm -rf "$BuildDir"
 cmake -S . -B "$BuildDir" -G "$Generator" --toolchain "$Toolchain" -DCMAKE_BUILD_TYPE="$BuildType" -DBUILD_TESTING=OFF
-cmake --build "$BuildDir" --config "$BuildType"
+cmake --build "$BuildDir" --config "$BuildType" -v
 
 ls -lA "$BuildDir"
