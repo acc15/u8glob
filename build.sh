f81f985308852fd27daab32f2152114636f69107
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2024-05-26 03:24:22 +0300

    build script

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+BuildDir="${BuildDir:-build}"
+BuildType="${BuildType:-Release}"
+Generator="${Generator:-Ninja}"
+Toolchain="${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake"
+
+rm -rf "$BuildDir"
+cmake -S . -B "$BuildDir" -G "$Generator" --toolchain "$Toolchain" -DCMAKE_BUILD_TYPE="$BuildType" -DBUILD_TESTING=OFF
+cmake --build "$BuildDir" --config "$BuildType"
+
+ls -lA "$BuildDir"
