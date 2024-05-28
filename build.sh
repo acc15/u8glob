9c8ed9f6ffcb7ffa3fcc706e034e690d451ed977
acc15 <vyacheslav.mayorov@gmail.com>
2024-05-28 12:54:31 +0300

    CMakeLists.txt improved

--- build.sh
+++ build.sh
@@ -1,12 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 BuildDir="${BuildDir:-build}"
 BuildType="${BuildType:-Release}"
 Generator="${Generator:-Ninja}"
 Toolchain="${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake"
+Testing="${Testing:-False}"
+
+ConfigureAppend="${ConfigureAppend:-}"
+if [[ $Arch ]]; then
+	ConfigureAppend="${ConfigureAppend} -A ${Arch}"
+fi
+
+BuildAppend="${BuildAppend:-}"
 
 rm -rf "$BuildDir"
-cmake -S . -B "$BuildDir" -G "$Generator" --toolchain "$Toolchain" -DCMAKE_BUILD_TYPE="$BuildType" -DBUILD_TESTING=OFF
-cmake --build "$BuildDir" --config "$BuildType" -v
+cmake -S . -B "$BuildDir" -G "$Generator" --toolchain "$Toolchain" -DCMAKE_BUILD_TYPE="$BuildType" -DBUILD_TESTING="${Testing}" $ConfigureAppend
+cmake --build "$BuildDir" --config "$BuildType" -v $BuildAppend
+ctest --test-dir "$BuildDir"
 
 ls -lA "$BuildDir"
