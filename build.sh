cd21e2ab7c756c474d2562bcceb527ddd7a03f1a
acc15 <vyacheslav.mayorov@gmail.com>
2024-05-28 23:08:52 +0300

    CMake install

--- build.sh
+++ build.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
+Clean="${Clean:-1}"
 BuildDir="${BuildDir:-build}"
 BuildType="${BuildType:-Release}"
 Generator="${Generator:-Ninja}"
 Toolchain="${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake"
 Testing="${Testing:-False}"
 
-ConfigureAppend="${ConfigureAppend:-}"
 if [[ $Arch ]]; then
 	ConfigureAppend="${ConfigureAppend} -A ${Arch}"
 fi
 
-BuildAppend="${BuildAppend:-}"
+if [[ ${Clean,,} =~ ^(yes|true|1)$ ]]; then
+	echo "Removing $BuildDir..."
+	rm -rf "$BuildDir"
+fi
 
-rm -rf "$BuildDir"
+echo "Configuring $BuildDir..."
 cmake -S . -B "$BuildDir" -G "$Generator" --toolchain "$Toolchain" -DCMAKE_BUILD_TYPE="$BuildType" -DBUILD_TESTING="${Testing}" $ConfigureAppend
+
+echo "Building $BuildDir..."
 cmake --build "$BuildDir" --config "$BuildType" -v $BuildAppend
+
+echo "Testing $BuildDir..."
 ctest --test-dir "$BuildDir"
 
 ls -lA "$BuildDir"
--- cmake/u8glob-config.cmake
+++
@@ -1,1 +0,0 @@
-include(${CMAKE_CURRENT_LIST_DIR}/u8glob-targets.cmake)
