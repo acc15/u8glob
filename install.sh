e10b93d8b5460e36eeccdfb20264a02bf07c5c18
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2024-05-29 23:43:30 +0300

    fix sudo

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 build_dir="build/$1"
+sudo=$(command -v sudo || echo "")
 rm -rf "$build_dir"
 cmake --workflow --preset $1
-sudo cmake --install "$build_dir" --config Debug
-sudo cmake --install "$build_dir" --config Release
+$sudo cmake --install "$build_dir" --config Debug
+$sudo cmake --install "$build_dir" --config Release
